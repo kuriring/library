ceefb6b33f146079da9b01f48884760b936ac036
ticktock <ticktock@outlook.kr>
2017-11-13 18:53:11 +0900

    merge

--- library_1009/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    error_reporting(E_ALL);
-    ini_set("display_error",1);
-    $db=new mysqli('localhost','root','dbpassword','dbname');
-    if($db->connect_error){
-        die();
-    }
-    $db->set_charset('utf8');
-?>
